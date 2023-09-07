7f45e9a743d572639ae0c422051e82e285c819f6
Promise <promisemaluleka15@gmail.com>
2023-09-07 08:15:41 -0700

    Project

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -255,34 +255,34 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	Elf64_Ehdr *header;
 
-	int o, r;
+	int o, r
 
-	o = open(argv[1], O_RDONLY)
+	o = open(argv[1], O_RDONLY);
 	if (o == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1])
-		exit(98)
+		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
+		exit(98);
 	}
-	header = malloc(sizeof(Elf64_Ehdr))
+	header = malloc(sizeof(Elf64_Ehdr));
 	if (header == NULL)
 	{
-		close_elf(o)
+		close_elf(o);
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
-		exit(98)
+		exit(98);
 	}
-	r = read(o, header, sizeof(Elf64_Ehdr))
+	r = read(o, header, sizeof(Elf64_Ehdr));
 	if (r == -1)
 	{
-		free(header)
-		close_elf(o)
-		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1])
-		exit(98)
+		free(header);
+		close_elf(o);
+		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
+		exit(98);
 	}
 	check_elf(header->e_ident)
 	printf("ELF Header:\n")
 	print_magic(header->e_ident)
 	print_class(header->e_ident)
- 	print_data(header->e_ident)
+	print_data(header->e_ident)
 	print_version(header->e_ident)
 	print_osabi(header->e_ident)
 	print_abi(header->e_ident)
